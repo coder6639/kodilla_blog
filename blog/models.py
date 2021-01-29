2c77123532fafa73018af7c51030a4764267c460
igi <github@6639.33mail.com>
2021-01-29 16:03:16 +0100

    Merge edit_post and add_post functions, move new function to models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
-from . import db
+from blog import db
 import datetime
+from flask import request, redirect, flash, url_for, render_template
+from blog.forms import EntryForm
 
 
 class Entry(db.Model):
@@ -9,3 +11,39 @@ class Entry(db.Model):
     pub_date = db.Column(db.DateTime, nullable=False,
                          default=datetime.datetime.utcnow)
     is_published = db.Column(db.Boolean, default=False)
+
+
+def create_or_edit(post_id):
+    if post_id == -1:
+        form = EntryForm()
+        errors = None
+        if request.method == "POST":
+            if form.validate_on_submit():
+                title = request.form["title"]
+                body = request.form["body"]
+                is_published = form.data.get("is_published")
+                post = Entry(title=title, body=body, is_published=is_published)
+                db.session.add(post)
+                db.session.commit()
+                flash("Post submitted successfully")
+                return redirect(url_for("index"))
+            else:
+                errors = form.errors
+        return render_template("post.html",
+                               form=form, errors=errors, type="new")
+    else:
+        entry = Entry.query.filter_by(id=post_id).first_or_404()
+        form = EntryForm(obj=entry)
+        errors = None
+        if request.method == "POST":
+            if form.validate_on_submit():
+                entry.title = request.form["title"]
+                entry.body = request.form["body"]
+                entry.is_published = form.data.get("is_published")
+                db.session.commit()
+                flash("Post edited successfully")
+                return redirect(url_for("index"))
+            else:
+                errors = form.errors
+        return render_template("post.html",
+                               form=form, errors=errors, type="edit")
--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,24 @@
 from blog import app
 from flask import render_template
+from blog.models import Entry
+from blog.models import create_or_edit
 
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    all_posts = Entry.query.filter_by(is_published=True).order_by(
+        Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/new/", methods=["GET", "POST"])
+def create_entry():
+    return create_or_edit(-1)
+
+
+@app.route("/edit/<int:post_id>/", methods=["GET", "POST"])
+def edit_post(post_id):
+    return create_or_edit(post_id)
 
 
 if __name__ == "__main__":
